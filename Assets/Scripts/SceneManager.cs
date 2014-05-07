73844370d508cf24aa8c191970c4be1e18823c35
Olga Cardenas <oh.cardenas@gmail.com>
2014-05-07 17:08:54 -0500

    Added base clases for creating service-locators

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour
+{
+	private IServiceA a;
+	private IServiceB b;
+
+	private IServiceLocator DemoLocator
+	{
+		get { return DemoServiceLocator.Instance; }
+	}
+
+	private void Awake ()
+	{
+		a = DemoLocator.GetService <IServiceA> ();
+		b = DemoLocator.GetService <IServiceB> ();
+	}
+
+	private void Start ()
+	{
+		Debug.Log (a.a);
+		Debug.Log (b.b);
+	}
+}
